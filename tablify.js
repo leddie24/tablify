9237f6293b32bb803406de12d4bfc630f0f62e77
leddie24 <leddie24@gmail.com>
2016-03-09 00:47:19 -0800

    add mouseover option

--- tablify.js
+++ tablify.js
@@ -1,27 +1,44 @@
 (function($) {
-   $.fn.tablify = function() {
+   $.fn.tablify = function(options) {
+      // Default options 
+      var defaults = {
+        mouseover: false
+      };
+
       var $this = $(this);
+      var opts = $.extend( {}, defaults, options );
+
+      var showTab = function(e) {
+         if (!$(e.target).hasClass('selected')) {
+            var idx = $(this).index();
+            $('.selected').removeClass('selected');
+            $('.tab-active').removeClass('tab-active');
+            $(this).addClass('selected')
+            $($this.selector).not(':eq(' + idx + ')').hide();
+            $($this.selector).eq(idx).show();
+         }
+      }
+
       var tabs = $('<ul></ul>').addClass('tabs');
       $this.each(function(idx, val) {
          tabs.append('<li class="test">' + $(val).attr('data-tab-title') + '</li>');
       });
 
       // Append tabs before first of class
-      $('.' + $(this)[0].className).first().before(tabs);
+      $($this.selector).first().before(tabs);
+      $($this.selector).parent().addClass('tab-container');
 
-      // Apply on click handler to show and remove tab content
-      $('.tabs li').on('click', function(e) {
-         if (!$(e.target).hasClass('selected')) {
-            var idx = $(this).index();
-            $('.selected').removeClass('selected');
-            $(this).addClass('selected');
-            $($this.selector).hide();
-            $($this.selector).eq(idx).show();
-         }
-      });
+      // Apply on handler to show and hide tab content
+      if (opts.mouseover) {
+         $('.tabs li').on('mouseover', showTab);
+      } else {
+         $('.tabs li').on('click', showTab);
+      }
 
       // Click first tab to hide others
-      $('.tabs li').eq(0).click();
+      $($(this).selector).hide();
+      $($(this).selector).eq(0).show();
+      $('.tabs li').eq(0).trigger('click');
       return this;
    }
 }(jQuery));
